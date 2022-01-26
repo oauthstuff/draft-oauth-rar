7aa2b89253b1ef31426469e0757f21d2d3158185
Brian Campbell <brian.d.campbell@gmail.com>
2022-01-26 10:49:51 -0700

    fix up (I think anyway?) make-v2.sh to take out the legacy xml2rfc v2 stuff

--- make-v2.sh
+++ make-v2.sh
@@ -2,7 +2,7 @@
 
 FILENAME=`grep value main.md | head -1 | cut -d'"' -f2`
 
-mmark -2 main.md > $FILENAME.xml
-`which xml2rfc` --legacy --html $FILENAME.xml
-`which xml2rfc` --legacy --text $FILENAME.xml
+mmark main.md > $FILENAME.xml
+`which xml2rfc` --html $FILENAME.xml
+`which xml2rfc` --text $FILENAME.xml
 
